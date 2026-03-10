758e1be07a4268f2257aa88053131ae1f68196ab
NITHISH-KUMAR-K-7 <grknithishkumar@gmail.com>
2026-03-10 22:53:28 +0530

    cors updates

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,9 @@ connectDB()
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:"https://expense-tracker-abpq6znwc-nithish-kumar-k-7s-projects.vercel.app/",
+}));
 app.use(express.json());
 
 app.use('/api/expense',authExpense)
